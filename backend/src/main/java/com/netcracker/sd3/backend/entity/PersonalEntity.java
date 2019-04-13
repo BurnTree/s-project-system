7736b014fa1a90300685d8f7fdd1cdeabafa8b37
Loko <mertvaya.1998@gmail.com>
2019-04-13 22:14:01 +0300

    Комит перед переделкой сущностей. Если ебнет, верни меня обратно:)

---
+++ backend/src/main/java/com/netcracker/sd3/backend/entity/PersonalEntity.java
@@ -0,0 +1,60 @@
+package com.netcracker.sd3.backend.entity;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "personal", schema = "projectdatabase", catalog = "")
+public class PersonalEntity {
+    private int id;
+    private String login;
+    private String password;
+
+    @Basic
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonalEntity that = (PersonalEntity) o;
+        return id == that.id &&
+                Objects.equals(login, that.login) &&
+                Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password);
+    }
+}
